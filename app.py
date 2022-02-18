946b38a5a52b4bbaf7bf13d330baac1a27e3006a
Ricky Palaguachi <rickypalaguachi@rickys-mbp.myfiosgateway.com>
2022-02-18 01:22:26 -0500

    allow posts

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def hello_world():
     return "hello!"
 
-@app.route("/api/body")
+@app.route("/api/body", methods=["POST"])
 def test_body():
     body = request.body
     return str(body)
