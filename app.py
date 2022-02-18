637c05e309d486fccf5b024e810e14fb2145dfaa
Ricky Palaguachi <rickypalaguachi@rickys-mbp.myfiosgateway.com>
2022-02-18 01:18:43 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/api/hello")
+def hello_world():
+    return "hello!"
+
+@app.route("/api/body")
+def test_body():
+    body = request.body
+    return str(body)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from beta import app
+
+if __name__ == "__main__":
+    app.run()
