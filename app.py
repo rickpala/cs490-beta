7d6a91059f3b2fe5d7fa6fdc130d330d23ae2988
Ricky Palaguachi <rickyp2413@gmail.com>
2022-02-20 01:29:06 -0500

    adjusted host for prod deployment

--- app.py
+++ app.py
@@ -11,9 +11,7 @@ def health():
     return "OK"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-else:
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-    app.run()
+    app.run(host='0.0.0.0')
