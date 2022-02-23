0d5406690c266df46b141b3e8a902c63255503de
Ricky Palaguachi <rickyp2413@gmail.com>
2022-02-23 18:42:13 -0500

    adjusted headers

--- login.py
+++ login.py
@@ -3,7 +3,7 @@ import flask
 import requests
 
 login = Blueprint('login', __name__)
-
+json_headers = {"Content-Type": "application/json"}
 @login.route('/api/login', methods=["POST"])
 def api_login():
     # receive body from frontend,
@@ -12,7 +12,7 @@ def api_login():
 
     # handoff to backend for validation
     backend = "https://cs100-testing-backend.herokuapp.com/validate_login"
-    r = requests.post(backend, json=body)
+    r = requests.post(backend, json=body, headers=json_headers)
 
     # grant/deny access via JSON credentials
     return flask.jsonify(r.text)
