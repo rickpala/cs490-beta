27e4bc102b5e14b1525e7d3dd780745a105539c6
Ricky Palaguachi <rickyp2413@gmail.com>
2022-02-23 12:47:47 -0500

    redirected login to new heroku backend

--- login.py
+++ login.py
@@ -7,23 +7,12 @@ login = Blueprint('login', __name__)
 @login.route('/api/login', methods=["POST"])
 def api_login():
     # receive body from frontend,
-    app.logger.debug(request.data)
-    app.logger.debug(request.json)
+    body = flask.request.data
 
     # handoff to backend for validation
-    backend = ("https://afsaccess4.njit.edu/~ps852/"
-               "alpha/functionality/validate_login.php")
-    r = requests.post(backend, json=request.json)
+    backend = ("https://cs100-testing-backend.herokuapp.com/validate_login")
+    r = requests.post(backend, json=body)
 
-    # check for student/teacher/error
-    app.logger.debug(r.status_code)
-
-    # grant/deny access via JSOownedN credentials
+    # grant/deny access via JSON credentials
     return r.json()
 
-@login.route('/api/teapot', methods=["POST"])
-def test_teapot():
-    r = requests.get("https://shielded-ridge-12044.herokuapp.com")
-    return r.text
-    
-    
