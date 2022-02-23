fa68e4c630bff49864df9152d064412ddfbcec55
Ricky Palaguachi <rickyp2413@gmail.com>
2022-02-23 13:07:01 -0500

    adjusted json handling

--- login.py
+++ login.py
@@ -7,12 +7,13 @@ login = Blueprint('login', __name__)
 @login.route('/api/login', methods=["POST"])
 def api_login():
     # receive body from frontend,
-    body = flask.request.data
+    body = flask.request.json
+    app.logger.debug(body)
 
     # handoff to backend for validation
-    backend = ("https://cs100-testing-backend.herokuapp.com/validate_login")
+    backend = "https://cs100-testing-backend.herokuapp.com/validate_login"
     r = requests.post(backend, json=body)
 
     # grant/deny access via JSON credentials
-    return r.json()
+    return flask.jsonify(r.text)
 
