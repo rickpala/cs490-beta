4836072e8d9c12087b3a50deeabd40184bcf8808
Ricky Palaguachi <rickyp2413@gmail.com>
2022-02-25 21:41:25 -0500

    added docs to /api/questions/new

--- questions.py
+++ questions.py
@@ -8,14 +8,33 @@ questions = Blueprint('questions', __name__)
 @questions.route("/api/questions/new", methods=["POST"])
 def question_new():
     """
-
+    Accepts a new question to be inserted into the question bank.
+    
+    Example JSON Body:
+        {
+            "teacherID": "teacherID_001",
+            "title": "Double it",
+            "description": "Write a function named `doubleIt`...",
+            "topic": "functions",
+            "difficulty": "easy",
+            "testCases": [{
+                "functionCall": "doubleIt(3)",
+                "expectedOutput": 6,
+                "type": "int"
+            }]
+        }
+    
+    Returns:
+        ({
+            "questionID": "questionID_001"
+        }, 201)
     """
     data = request.json
     app.logger.debug(data)
     # validate all keys exist
     req_keys = ["title", "description", "topic", "difficulty", "testCases"]
     if keys_missing(req_keys, data):
-        return flask.jsonify({"error": "missing keys"}), 400
+        return flask.jsonify({"error": "missing keys in form data"}), 400
 
     if keys_missing(["functionCall", "expectedOutput", "type"], data["testCases"]):
         return flask.jsonify({"error": "missing keys in testCases"}), 400
