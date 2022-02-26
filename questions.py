562d079cc911c5d43e3785e408b2924629a15192
Ricky Palaguachi <rickyp2413@gmail.com>
2022-02-25 20:45:13 -0500

    early return

--- questions.py
+++ questions.py
@@ -17,6 +17,8 @@ def question_new():
     if keys_missing(["callSignature", "answer", "type"], data["testCases"]):
         pass # TODO: Validate testCases
 
+    return flask.jsonify({}), 200
+
     # hand off to database to insert 
     r = requests.post(f"{BACKEND_URL}/new_question", json=data)
 
